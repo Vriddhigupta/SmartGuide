470eb46593e3614aa0013611d8b8cdcb531126fb
Vriddhigupta <vriddhigupta898@gmail.com>
2021-05-10 22:06:15 +0530

    Initial import

---
+++ app/src/main/java/com/example/nearmedemo/Activity/ForgetActivity.java
@@ -0,0 +1,74 @@
+package com.example.nearmedemo.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.nearmedemo.R;
+import com.example.nearmedemo.databinding.ActivityForgetBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgetActivity extends AppCompatActivity {
+
+    private ActivityForgetBinding binding;
+    EditText edtEmail;
+    Button btnResetPassword;
+    private FirebaseAuth mAuth;
+    ImageView btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityForgetBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        edtEmail = (EditText) findViewById(R.id.email);
+        btnResetPassword = (Button) findViewById(R.id.btnForgetPassword);
+        btnBack = (ImageView) findViewById(R.id.btnBack);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edtEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ForgetActivity.this, "Check email to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ForgetActivity.this, "Fail to send reset password email!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+
+    }
